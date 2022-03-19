6503d420b8fdf22ce7389394aeb7779089732034
Juliano C L Santos <foxports@hotmail.com>
2022-03-19 16:14:48 -0300

    Iniciando com as constraints

--- AluraViagens/HomeTableViewHeader.swift
+++ AluraViagens/HomeTableViewHeader.swift
@@ -14,5 +14,8 @@ class HomeTableViewHeader: UIView {
     @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var BannerImageView: UIImageView!
     
+    func configuraView() {
+        headerView.backgroundColor = UIColor(red: 30.0/255.00, green: 59.0/255.0, blue: 119.0/255.0, alpha: 1)
+    }
     
 }
