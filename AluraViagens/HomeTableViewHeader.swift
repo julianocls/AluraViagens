e1f57cdbbf8628f3c3f3d6cc67da9797b834d923
Juliano C L Santos <foxports@hotmail.com>
2022-03-19 15:16:28 -0300

    Exibindo o header na TableView

---
+++ AluraViagens/HomeTableViewHeader.swift
@@ -0,0 +1,20 @@
+//
+//  HomeTableViewHeader.swift
+//  AluraViagens
+//
+//  Created by Juliano Santos on 19/3/22.
+//
+
+import UIKit
+
+class HomeTableViewHeader: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
