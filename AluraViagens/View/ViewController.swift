6be722fe05b08b1d65fe42dd50e35b70f203b53a
Juliano C L Santos <foxports@hotmail.com>
2022-03-20 16:13:32 -0300

    Registrando a célula na TableView

--- AluraViagens/View/ViewController.swift
+++ AluraViagens/View/ViewController.swift
@@ -13,10 +13,14 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configuraTableView()
+        view.backgroundColor = UIColor(red: 30.0/255.0, green: 59.0/255.0, blue: 119.0/255.0, alpha: 1)
+    }
+    
+    func configuraTableView() {
+        viagensTableView.register(UINib(nibName: "ViagemTableViewCell", bundle: nil), forCellReuseIdentifier: "ViagemTableViewCell")
         viagensTableView.dataSource = self
         viagensTableView.delegate = self
-        
-        view.backgroundColor = UIColor(red: 30.0/255.0, green: 59.0/255.0, blue: 119.0/255.0, alpha: 1)
     }
     
 }
@@ -28,11 +32,8 @@ extension ViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
-        
-        cell.textLabel?.text = "Viagem \(indexPath.row)"
-        
-        return cell
+        guard let celulaViagem = tableView.dequeueReusableCell(withIdentifier: "ViagemTableViewCell") as? ViagemTableViewCell else { fatalError("Error to create ViagemTableViewCell") }
+        return celulaViagem
     }
     
 }
@@ -50,4 +51,7 @@ extension ViewController: UITableViewDelegate {
         return 300
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 400
+    }
 }
