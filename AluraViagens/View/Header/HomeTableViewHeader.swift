7d6388732097fba82a32ecf5555d281bb86139e8
Juliano C L Santos <foxports@hotmail.com>
2022-03-20 13:55:37 -0300

    Arredondando view do banner

--- AluraViagens/View/Header/HomeTableViewHeader.swift
+++ AluraViagens/View/Header/HomeTableViewHeader.swift
@@ -13,9 +13,16 @@ class HomeTableViewHeader: UIView {
     @IBOutlet weak var tituloLabel: UILabel!
     @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var BannerImageView: UIImageView!
+    @IBOutlet weak var bannerView: UIView!
     
     func configuraView() {
         headerView.backgroundColor = UIColor(red: 30.0/255.00, green: 59.0/255.0, blue: 119.0/255.0, alpha: 1)
+        
+        bannerView.layer.cornerRadius = 10
+        bannerView.layer.masksToBounds = true
+        
+        headerView.layer.cornerRadius = 100
+        headerView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
     }
     
 }
