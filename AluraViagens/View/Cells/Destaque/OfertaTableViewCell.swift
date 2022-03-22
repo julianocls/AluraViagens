0e416de155f0a37fae3a88e17674cf2e396fb09d
Juliano C L Santos <foxports@hotmail.com>
2022-03-22 17:50:44 -0300

    Cria célula de oferta; Grupos grupos no grupo Cells

---
+++ AluraViagens/View/Cells/Destaque/OfertaTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  OfertaTableViewCell.swift
+//  AluraViagens
+//
+//  Created by Juliano Santos on 22/3/22.
+//
+
+import UIKit
+
+class OfertaTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
