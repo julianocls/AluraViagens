7bf392ec7e6dd3a2e38df1092bf041e252616e59
Juliano C L Santos <foxports@hotmail.com>
2022-03-24 12:18:08 -0300

    Criando outlets collection para banner

--- AluraViagens/View/Cells/Destaque/OfertaTableViewCell.swift
+++ AluraViagens/View/Cells/Destaque/OfertaTableViewCell.swift
@@ -9,15 +9,13 @@ import UIKit
 
 class OfertaTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+    // MARK: -- IBOutlets
+    @IBOutlet var viagemImages: [UIImageView]!
+    @IBOutlet var tituloViagemLabels: [UILabel]!
+    @IBOutlet var subtituloViagemLabels: [UILabel]!
+    @IBOutlet var precoSemDescontoLabels: [UILabel]!
+    @IBOutlet var precoLabels: [UILabel]!
+    
+    
     
 }
